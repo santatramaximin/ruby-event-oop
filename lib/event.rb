44a9d69976303fb8fee38b1ceb556bae19e78559
santatramaximin <santatramaximin@gmail.com>
2019-01-21 17:15:01 +0100

    ajout de devoir lundi

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,62 @@
+require 'pry'
+require 'time'
+
+
 class Event
 
+	attr_accessor :start_date,:length,:title,:attendees
+
+
+	def initialize (start_date, length,	title, attendees)
+		@start_date = Time.parse(start_date)
+		@length = length.to_i
+		@title = title
+		@attendees = attendees
+
+		
+	end
+	
+	def postpone_24h 
+		@start_date = @start_date +60*60*24
+	end
+	
+
+
+	def end_date
+		@end_date = @start_date + 60*@length
+	end
+
+
+	def is_past
+		return @start_date < Time.now ? true : false
+	end
+
+
+	def is_future
+		#return @start_date > Time.now ? true : false
+	#end (samy mety ireo)
+	!self.is_past
+	end
+
+	def is_soon
+		(start_date - Time.now)/60 <= 30 ? true : false if is_future
+	end
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
+
+binding.pry
+puts "end of file"
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,15 @@
 class User
 
+	attr_accessor :name, :email
+	@@all_user = []
+
+	def initialize(email_to_save)
+		@email = email_to_save
+		@@all_user << self
+	end
+
+	def self.all
+		@@all_user
+	end
+
 end
